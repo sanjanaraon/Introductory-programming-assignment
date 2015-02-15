ebdf5270f28d5521f2d4ff612dbe2040ec608470
sanjana rao n <sanjanar@thoughtworks.com>
2015-02-15 20:12:02 +0530

    program prints name in the line number equal to the number entered

--- src/DiamondWithName.java
+++ src/DiamondWithName.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 /**
  * Created by sanjanar on 15/02/15.
  */
 public class DiamondWithName {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number = input.nextInt();
+        for (int i = 0; i < number-1; i++) {
+            for (int space = 0; space < number - i - 1; space++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i * 2 + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("Bill");
+        for (int i = number - 1; i >= 1; i--) {
+            for (int space = 0; space < (number - i); space++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int k = 1; k < i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
