7b7c99649de4e4d61a7608884dc7329a2128eb17
sanjana rao n <sanjanar@thoughtworks.com>
2015-02-15 17:47:53 +0530

    program to print '*' horizontally

---
+++ src/PrintNAsteriskHorizontally.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Created by sanjanar on 15/02/15.
+ */
+public class PrintNAsteriskHorizontally {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number=input.nextInt();
+        for(int i=1;i<=number;i++){
+            System.out.print("* ");
+        }
+    }
+}
