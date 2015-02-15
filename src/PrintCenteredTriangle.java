8befb62c8082d414ea782415aec4a2e2304d691d
sanjana rao n <sanjanar@thoughtworks.com>
2015-02-15 18:56:54 +0530

    added full version to print centered triangle

--- src/PrintCenteredTriangle.java
+++ src/PrintCenteredTriangle.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by sanjanar on 15/02/15.
  */
 public class PrintCenteredTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number = input.nextInt();
+        for (int i = 0; i <number; i++) {
+            for(int space=0;space<number-i-1;space++){
+                System.out.print(" ");
+            }
+            for (int j = 0;j<i*2+1 ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
+
